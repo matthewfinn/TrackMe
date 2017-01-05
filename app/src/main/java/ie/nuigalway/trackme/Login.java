927fcc03aaa44be7cf2a972921b8582d371e8c2d
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-05 19:34:26 +0000

    messing with registration UI

--- app/src/main/java/ie/nuigalway/trackme/Login.java
+++ app/src/main/java/ie/nuigalway/trackme/Login.java
@@ -1,24 +1,27 @@
+/**
+ * Copyright (c) 2017 NUI Galway
+ * Author:       Matthew Finn
+ */
+
 package ie.nuigalway.trackme;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         Intent intent = getIntent();
-        String message = intent.getStringExtra(Register.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(Register.REG_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
--- app/src/main/java/ie/nuigalway/trackme/Register.java
+++ app/src/main/java/ie/nuigalway/trackme/Register.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2017 NUI Galway
+ * Author: Matthew Finn
+ */
+
 package ie.nuigalway.trackme;
 
 import android.content.Intent;
@@ -6,18 +11,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 public class Register extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String REG_MESSAGE = "ie.nuigalway.trackme.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
 
+    //This method is called when the Register button is clicked
     public void registerUser(View view) {
         Intent intent = new Intent(this, Login.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.edit_name);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(REG_MESSAGE, message);
         startActivity(intent);
     }
 }
