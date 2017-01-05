3f5b7b2f0bf01ea98f926470f0a74cf4aa62d968
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-05 00:56:38 +0000

    first commit

---
+++ app/src/main/java/ie/nuigalway/trackme/Register.java
@@ -0,0 +1,23 @@
+package ie.nuigalway.trackme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+public class Register extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void registerUser(View view) {
+        Intent intent = new Intent(this, Login.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
