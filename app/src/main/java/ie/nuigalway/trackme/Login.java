52c3fc4b46f14e6644074ca8e567e84a621ff7fe
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-14 23:31:40 +0000

    server stuff started

--- app/src/main/java/ie/nuigalway/trackme/Login.java
+++ app/src/main/java/ie/nuigalway/trackme/Login.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class Login extends AppCompatActivity {
@@ -22,6 +23,24 @@ public class Login extends AppCompatActivity {
     }
 
     public void checkLogin(View view){
+
+        EditText username = (EditText)findViewById(R.id.login_username);
+        EditText password = (EditText)findViewById(R.id.login_username);
+
+        //get username and password from server and check them.
+        String usernameRetrieved;
+        String passwordRetrieved;
+
+
+        if(username.getText().toString().equals("") && password.getText().toString().equals("")){
+            //Successful login
+
+        }else{
+
+
+        }
+
+
         Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
     }
