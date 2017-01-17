0e173b116c7f392da379263534db5ebc861ee6c2
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-17 22:39:34 +0000

    app url config for server side

---
+++ app/src/main/java/ie/nuigalway/trackme/application/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package ie.nuigalway.trackme.application;
+
+/**
+ * Created by matthew on 17/01/2017.
+ */
+
+public class ApplicationConfig {
+    public static String REGISTRATION_URL = "http://danu6.it.nuigalway.ie/trackMeServer/register.php";
+    public static String LOGIN_URL = "http://danu6.it.nuigalway.ie/trackMeServer/login.php";
+}
