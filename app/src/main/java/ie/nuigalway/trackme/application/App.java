d3d718d19c1e94c658f2d4dc67a2a255cc741a03
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-18 00:36:54 +0000

    application controller aded with Volley object handlers

---
+++ app/src/main/java/ie/nuigalway/trackme/application/App.java
@@ -0,0 +1,53 @@
+package ie.nuigalway.trackme.application;
+
+/**
+ * Created by matthew on 17/01/2017.
+ * Class that creates core Volley objects to handle data transmission.
+ */
+import android.app.Application;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import android.text.TextUtils;
+
+
+public class App extends Application {
+
+    public final static String TAG = App.class.getSimpleName();
+    private static App app;
+    private RequestQueue rq;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+    }
+
+    public static synchronized App getInstance() {
+        return app;
+    }
+
+    public RequestQueue getRQ() {
+        if (rq == null) {
+            rq = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return rq;
+    }
+
+    public <T> void addToRQ(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRQ().add(req);
+    }
+
+    public <T> void addToTQ(Request<T> req) {
+        req.setTag(TAG);
+        getRQ().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (rq != null) {
+            rq.cancelAll(tag);
+        }
+    }
+}
