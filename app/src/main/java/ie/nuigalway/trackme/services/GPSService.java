f6182e213e9c4397d7a22380b4458f30e860e1f7
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-23 21:04:03 +0000

    started GPS service

--- app/src/main/java/ie/nuigalway/trackme/services/GPSService.java
+++ app/src/main/java/ie/nuigalway/trackme/services/GPSService.java
@@ -2,9 +2,17 @@ package ie.nuigalway.trackme.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.IBinder;
 
 public class GPSService extends Service {
+
+    private static final String TAG = "GPSLOCATIONLISTENER";
+    private LocationManager lm = null;
+    private static final int LOCATION_INTERVAL = 200;
+    private static final float LOCATION_DISTANCE = 10f;
+
+
     public GPSService() {
     }
 
