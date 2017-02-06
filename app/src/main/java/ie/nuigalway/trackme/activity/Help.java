3f792fa8b1db7c145fe4e94481135d115b5afe3c
Matthew Finn <m.finn11@nuigalway.ie>
2017-02-06 20:29:58 +0000

    added app layout

---
+++ app/src/main/java/ie/nuigalway/trackme/activity/Help.java
@@ -0,0 +1,16 @@
+package ie.nuigalway.trackme.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.nuigalway.trackme.R;
+
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
