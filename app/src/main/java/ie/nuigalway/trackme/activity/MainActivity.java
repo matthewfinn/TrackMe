b751d58d9e281037c3a536e0d7af26d3320d3e40
Matthew Finn <m.finn11@nuigalway.ie>
2017-02-21 15:27:52 +0000

    Converting to fragments, login authentication working

---
+++ app/src/main/java/ie/nuigalway/trackme/activity/MainActivity.java
@@ -0,0 +1,139 @@
+package ie.nuigalway.trackme.activity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import ie.nuigalway.trackme.R;
+import ie.nuigalway.trackme.fragment.LoginFragment;
+import ie.nuigalway.trackme.fragment.MenuFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, LoginFragment.OnFragmentInteractionListener,
+        MenuFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = LoginFragment.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_menu_nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        if (id == R.id.nav_camera) {
+            fragmentClass = LoginFragment.class;
+        } else if (id == R.id.nav_gallery) {
+            fragmentClass = MenuFragment.class;
+        } else if (id == R.id.nav_slideshow) {
+            fragmentClass = LoginFragment.class;
+        } else if (id == R.id.nav_manage) {
+            fragmentClass = LoginFragment.class;
+        } else if (id == R.id.nav_share) {
+            fragmentClass = LoginFragment.class;
+        } else if (id == R.id.nav_send) {
+            fragmentClass = LoginFragment.class;
+        }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+}
---
+++ app/src/main/java/ie/nuigalway/trackme/fragment/LoginFragment.java
@@ -0,0 +1,266 @@
+package ie.nuigalway.trackme.fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ie.nuigalway.trackme.R;
+import ie.nuigalway.trackme.application.App;
+import ie.nuigalway.trackme.application.AppConfig;
+import ie.nuigalway.trackme.helper.LocalDBHandler;
+import ie.nuigalway.trackme.helper.SessionManager;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment implements View.OnClickListener /*, TextWatcher*/ {
+    private static final String TAG = LoginFragment.class.getSimpleName();
+
+    private EditText email, password;
+    private Button login_button;
+    private SessionManager sm;
+    private LocalDBHandler db;
+    private ProgressDialog pd;
+    private String em, pw;
+    private OnFragmentInteractionListener mListener;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        login_button = (Button) view.findViewById(R.id.login_button);
+        login_button.setOnClickListener(this);
+
+        email = (EditText) view.findViewById(R.id.login_email);
+      //  email.addTextChangedListener(this);
+        password = (EditText) view.findViewById(R.id.login_password);
+//        password.addTextChangedListener(this);
+
+        pd = new ProgressDialog(getContext());
+        pd.setCancelable(false);
+
+        db = new LocalDBHandler(getContext());
+        sm = new SessionManager(getContext());
+
+        return view;
+    }
+
+
+//    @Override
+//    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//    }
+//
+//    @Override
+//    public void onTextChanged(CharSequence s, int start, int before, int count) {
+//        em = email.getText().toString().trim();
+//        pw = password.getText().toString().trim();
+//    }
+//
+//    @Override
+//    public void afterTextChanged(Editable s) {
+//
+//        em = email.getText().toString().trim();
+//        pw = password.getText().toString().trim();
+//
+//    }
+    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri) {
+//
+//
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+
+                attemptLogin();
+
+
+
+                break;
+        }
+    }
+    private void attemptLogin() {
+
+        em = email.getText().toString().trim();
+        pw = password.getText().toString().trim();
+
+        // else if block(s) to verify is user entered correct information.
+
+        if (!em.isEmpty() && !pw.isEmpty()) {
+
+            // Verify user login using details provided
+            verifyLogin(em, pw);
+
+        }else if (em.isEmpty()&&pw.isEmpty()){
+
+
+            Toast.makeText(getActivity(),
+                    "Please Enter Email Address & Password To Log In", Toast.LENGTH_LONG)
+                    .show();
+        }
+        else if (em.isEmpty()){
+            Toast.makeText(getContext(),
+                    "Please Enter Email Address", Toast.LENGTH_LONG)
+                    .show();
+        }else if(pw.isEmpty()){
+            Toast.makeText(getContext(),
+                    "Please Enter Password", Toast.LENGTH_LONG)
+                    .show();
+        }
+        else{
+
+            Toast.makeText(getContext(),
+                    "Incorrect Login Details", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void verifyLogin(final String email, final String password){
+
+        String req = "req_login";
+
+        pd.setMessage("Logging In");
+
+        if(!pd.isShowing()){
+            pd.show();
+
+        }
+
+        StringRequest r = new StringRequest(Request.Method.POST, AppConfig.LOGIN_URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String res) {
+
+                Log.d(TAG, "Response: " + res.toString());
+                if (pd.isShowing()) {
+                    pd.dismiss();
+                }
+                try {
+
+                    JSONObject j = new JSONObject(res);
+                    boolean error = j.getBoolean("error");
+
+                    if (!error) {
+                        String uid = j.getString("uid");
+                        JSONObject user = j.getJSONObject("user");
+
+                        String fn = user.getString("first_name");
+                        String sn = user.getString("surname");
+                        String em = user.getString("email");
+                        String ph = user.getString("phone_no");
+                        String cr = user.getString("created_at");
+
+                        sm.startLoginSession(true, fn, sn, em, ph);
+                        db.addUser(uid, fn, sn, em, ph, cr);
+
+                        Fragment frag = new MenuFragment();
+                        FragmentTransaction ft = getFragmentManager().beginTransaction();
+                        ft.replace(R.id.fragment_login, frag);
+                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                        ft.addToBackStack(null);
+                        ft.commit();
+
+                    } else {
+                        String err = j.getString("error_msg");
+                        Toast.makeText(getContext(), "On Response: "+err , Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException jse) {
+
+                    jse.printStackTrace();
+                    Toast.makeText(getContext(), "On Response Error" + jse.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError e){
+
+                Log.e(TAG,"Login : "+ e.getMessage());
+                Toast.makeText(getContext(),"On Error Response: "+ e.getMessage(), Toast.LENGTH_LONG).show();
+
+                if(pd.isShowing()) {
+
+                    pd.dismiss();
+                }
+            }
+
+        }){
+
+            @Override
+            protected Map<String, String> getParams(){
+
+                Map<String,String> p = new HashMap<String,String>();
+                p.put("email", email);
+                p.put("password",password);
+
+
+                return p;
+            }
+        };
+
+        App.getInstance().addToRQ(r, req);
+
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
